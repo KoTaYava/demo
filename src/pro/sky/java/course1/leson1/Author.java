1fc90a745d15ca76afd60bfb049d124691dfc7f6
KoTaYava <146347467+KoTaYava@users.noreply.github.com>
2023-11-11 12:09:11 +0300

    dz4

--- src/pro/sky/java/course1/leson1/Author.java
+++ src/pro/sky/java/course1/leson1/Author.java
@@ -21,7 +21,7 @@ public class Author {
     }
     @Override
     public boolean equals(Object other) {
-        if (this==other) return true;
+        if (this == other) return true;
         if (other == null || this.getClass() != other.getClass()) {
             return false;
         }
